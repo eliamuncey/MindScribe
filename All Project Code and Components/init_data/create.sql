692464ce221549a1b2ffdaefd6ee0ce5083b68ac
Elia Muncey <elmu7954@colorado.edu>
2023-04-20 12:16:00 -0600

    added journals table

--- All Project Code and Components/init_data/create.sql
+++ All Project Code and Components/init_data/create.sql
@@ -4,6 +4,13 @@ username VARCHAR(50) PRIMARY KEY,
 password CHAR(60) NOT NULL
 );
 
+DROP TABLE IF EXISTS journals CASCADE;
+CREATE TABLE journals(
+journal_id SERIAL PRIMARY KEY,
+journal_name,
+username VARCHAR(50) REFERENCES users(username),
+);
+
 DROP TABLE IF EXISTS entries CASCADE;
 CREATE TABLE entries(
 entry_id SERIAL PRIMARY KEY,
@@ -13,5 +20,6 @@ entry_date DATE,
 raw_text TEXT,
 formatted_text TEXT,
 summary_text TEXT,
-entry_mood INT
+entry_mood INT,
+journal_id INT REFERENCES journals(journal_id)
 );
