6f974f3b35763ac0af31adb386652c559aca5d72
Blake DeHaas <blde8334@gmail.com>
2023-04-27 07:03:05 -0600

    Implemented ChatGPT API and format function to notes

--- All Project Code and Components/index.js
+++ All Project Code and Components/index.js
@@ -71,32 +71,34 @@ const openai = new OpenAIApi(configuration);
 // <!-- Section 4 : API Routes -->
 // *****************************************************
 
-async function runCompletion() {
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: "Hi ChatGPT how are you today?",
+// async function runCompletion() {
+//   const completion = await openai.createCompletion({
+//     model: "text-davinci-003",
+//     prompt: "Hi ChatGPT how are you today?",
+//   });
+
+//   console.log(completion.data.choices[0].text);
+// }
+
+// runCompletion();
+
+app.post('/format', async function (req, res) { //async function to await for ChatGPT reply
+  const { raw_text, entry_id } = req.body; //seperates data
+  
+  const prompt = `Format the following entry for increased readability using appropriate punctuation, capitalization, spacing, paragraph breaks, and correct typos, reply only with the formatted version of the text:\n\n${raw_text}`;
+  
+  const completion = await openai.createCompletion({ //createCompletion is a OpenAI keyword to complete a prompt, await for ChatGPT reply
+    model: "text-davinci-003", //text model being used
+    prompt: prompt, //prompt set above
+    max_tokens: 2048, //max number of word chunks in a single output
+    n: 1, //max number of outputs (only need one)
   });
 
-  console.log(completion.data.choices[0].text);
-}
-
-runCompletion();
-
-app.post('/format', function (req, res) {
-//  const query =
-//  'INSERT INTO entries (entry_title, raw_text) VALUES ($1, $2) RETURNING *;';
-  // INSERT INTO entries (entry_title, raw_text, journal_id) VALUES ($1, $2, $3) RETURNING *;';
-// db.any(query, [
- //   req.body.entry_title,
-//    req.body.raw_text,
-    // req.body.journal_id
- //])
-    (function (data) {
-      const completion = openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: "Hi ChatGPT are you there?",
-      })
-      console.log(completion.data.choices[0].text);
+  const formatted_text = completion.data.choices[0].text; //pulling out the text of the first choice from data from completion object
+  
+  const updateQuery = 'UPDATE entries SET raw_text = $1 where entry_id = $2;'; //update entry content
+  db.any(updateQuery, [formatted_text, entry_id])
+    .then(function(data){
       res.status(200).json({
         status: 'success',
         data: data,
@@ -111,6 +113,8 @@ app.post('/format', function (req, res) {
       });
     });
 });
+
+
 
 
 app.get('/welcome', (req, res) => { //example test case function
@@ -177,18 +181,37 @@ const auth = (req, res, next) => {
 // Authentication Required
 app.use(auth);
 
-app.get("/createnewnote", (req, res) => {
-  res.render("pages/createnewnote");
-})
+app.get('/createnewnote', function (req, res) {
+  const query =
+    'SELECT * FROM journals;';
+  db.any(query)
+    .then(function (data) {
+      res.render('pages/createnewnote', {
+        journals: data
+      });
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(400).json({
+        status: 'error',
+        message: 'An error occurred while creating a new note'
+      });
+    });
+});
 
 app.post('/savenote', function (req, res) {
   const query =
-  'INSERT INTO entries (entry_title, raw_text) VALUES ($1, $2) RETURNING *;';
-  // INSERT INTO entries (entry_title, raw_text, journal_id) VALUES ($1, $2, $3) RETURNING *;';
+  // 'INSERT INTO entries (entry_title, raw_text, username, entry_date) VALUES ($1, $2, $3, $4) RETURNING *;';
+  'INSERT INTO entries (entry_title, raw_text, username, entry_date, journal_id) VALUES ($1, $2, $3, $4, $5) RETURNING *;';
+
+  const date = new Date().toISOString();  // get the current date as an ISO string
+
   db.any(query, [
     req.body.entry_title,
     req.body.raw_text,
-    // req.body.journal_id
+    req.session.user.username,
+    date,
+    req.body.journal_id
   ])
     .then(function (data) {
       res.status(200).json({
@@ -208,8 +231,8 @@ app.post('/savenote', function (req, res) {
 
 app.get('/opennote', (req, res) => { 
   const entryId = req.query['entry-id'];
-  // var entryId = req.query.id;
-  const query = 'SELECT * FROM entries WHERE entry_id = $1'; // SQL query to retrieve entry with correct entry_id
+  const query = 'SELECT entries.*, journals.journal_title FROM entries LEFT JOIN journals ON entries.journal_id = journals.journal_id WHERE entries.entry_id = $1'; 
+  // SQL query to retrive journal info from correct entry_id, LEFT JOIN for entries without a journal_id
   db.any(query, [entryId])
     .then(function (data) {
       res.render('pages/opennote', {entry: data}); // Pass the 'data' to the 'entry' variable
@@ -309,10 +332,16 @@ app.get('/journal', (req, res) => {
 // Get the entry from the database then enter the edit page with the contents of the entry
 app.get('/edit', (req, res) => {
   var id = req.query.id;  // get the ID from the ID query parmater in the URL
-  const query = "SELECT * FROM entries where entry_id = $1;"; // SQL query to retrieve all entries
-  db.any(query, [id]) 
+  const entryQuery = 'SELECT entries.*, journals.journal_title FROM entries LEFT JOIN journals ON entries.journal_id = journals.journal_id WHERE entries.entry_id = $1';
+  const journalQuery = 'SELECT * FROM journals;';
+  db.task(function (t) {
+    return t.batch([
+      t.any(entryQuery, [id]), // execute the entryQuery and pass the entry_id parameter
+      t.any(journalQuery), // execute the journalQuery
+    ]);
+  })
     .then(function (data) {
-      res.render('pages/edit', {results: data}); // Pass the 'data' to the 'results' variable in the home page
+      res.render('pages/edit', {results: data[0], journals: data[1]}); // Pass the 'data' to the 'results' variable and 'journals' to the 'journals' variable
     })
     .catch(function (err) {
       console.error(err);
@@ -323,15 +352,42 @@ app.get('/edit', (req, res) => {
     });
 });
 
+
+// Get the journal entry from the database then enter the edit journal page with the contents of the journal
+app.get('/editjournal', (req, res) => {
+  var id = req.query.id;  // get the ID from the ID query parmater in the URL
+  const query = "SELECT * FROM journals where journal_id = $1;"; // SQL query to retrieve all entries
+  db.any(query, [id]) 
+    .then(function (data) {
+      res.render('pages/editjournal', {results: data}); // Pass the 'data' to the 'results' variable in the home page
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).json({
+        status: 'error',
+        message: 'An error occurred while fetching notes',
+      });
+    });
+});
+
+
 // Save an edited note - update the text in the database
 app.post('/updatenote', function (req, res) {
-  const query =
-    'UPDATE entries SET entry_title = $1, raw_text = $2 where entry_id = $3;';
-  db.any(query, [
-  	req.body.title,
-    req.body.text,
-    req.body.id
+  const journalQuery = 'SELECT * FROM journals WHERE journals.journal_title = $1;';
+  const noJournalQuery = 'UPDATE entries SET entry_title = $1, raw_text = $2, journal_id = null where entry_id = $3;';
+  const updateQuery = 'UPDATE entries SET entry_title = $1, raw_text = $2, journal_id = $3 where entry_id = $4;';
+  if(req.body.journal_title != "No Journal") {
+  db.one(journalQuery, [
+    req.body.journal_title
   ])
+  .then(function (data){
+    db.any(updateQuery, [
+      req.body.title,
+      req.body.text,
+      data.journal_id,
+      req.body.id
+    ])
+  })
     .then(function (data) {
       res.redirect('/home');   // go to the home page
     })
@@ -340,6 +396,45 @@ app.post('/updatenote', function (req, res) {
       res.status(500).json({
         status: 'error',
         message: 'An error occurred while saving the note',
+      });
+    })
+  }
+  else {
+      db.any(noJournalQuery, [
+        req.body.title,
+        req.body.text,
+        req.body.id
+      ])
+      .then(function (data) {
+        res.redirect('/home');   // go to the home page
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.status(500).json({
+          status: 'error',
+          message: 'An error occurred while saving the note',
+        });
+      });
+  }
+});
+
+// Save an edited note - update the text in the database
+app.post('/updatejournal', function (req, res) {
+  const query =
+    'UPDATE journals SET journal_title = $1, journal_description = $2 where journal_id = $3;';
+  db.any(query, [
+  	req.body.title,
+    req.body.description,
+    req.body.id
+  ])
+    .then(function (data) {
+      res.redirect('/journal');   // go to the home page
+    })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).json({
+        status: 'error',
+        message: 'An error occurred while updating the journal',
       });
     });
 });
@@ -357,6 +452,24 @@ app.get('/deletenote', function (req, res) {
       res.status(500).json({
         status: 'error',
         message: 'An error occurred while deleting the note',
+      });
+    });
+});
+
+app.get('/deletejournal', function (req, res) { //delete journal
+  var id = req.query.id;
+  const updateQuery ='UPDATE entries SET journal_id = null WHERE journal_id = $1;';
+  const deleteQuery ='DELETE FROM journals WHERE journal_id = $1;';
+  db.task(async (t) => {
+    await t.none(updateQuery, [id]); // Update all entries with journal_id = null
+    await t.none(deleteQuery, [id]); // Delete journal
+    res.redirect('/journal'); // Redirect to journal page
+  })
+    .catch(function (err) {
+      console.error(err);
+      res.status(500).json({
+        status: 'error',
+        message: 'An error occurred while deleting the journal',
       });
     });
 });
