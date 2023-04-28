f78727b22ce4c245554f0d9b420529dcc17e5e65
Elia Muncey <elmu7954@colorado.edu>
2023-04-27 23:22:57 -0600

    changed primary keys in database

--- All Project Code and Components/init_data/create.sql
+++ All Project Code and Components/init_data/create.sql
@@ -1,7 +1,12 @@
 DROP TABLE IF EXISTS users CASCADE;
+-- CREATE TABLE users(
+-- username VARCHAR(50) PRIMARY KEY,
+-- user_id SERIAL,
+-- password CHAR(60) NOT NULL
+-- );
 CREATE TABLE users(
-username VARCHAR(50) PRIMARY KEY,
-user_id SERIAL,
+user_id SERIAL PRIMARY KEY,
+username VARCHAR(50) NOT NULL,
 password CHAR(60) NOT NULL
 );
 
@@ -10,13 +15,15 @@ CREATE TABLE journals(
 journal_id SERIAL PRIMARY KEY,
 journal_title TEXT,
 journal_description TEXT,
-username VARCHAR(50) REFERENCES users(username)
+user_id INT REFERENCES users(user_id)
+-- username VARCHAR(50) REFERENCES users(username)
 );
 
 DROP TABLE IF EXISTS entries CASCADE;
 CREATE TABLE entries(
 entry_id SERIAL PRIMARY KEY,
-username VARCHAR(50) REFERENCES users (username),
+user_id INT REFERENCES users(user_id),
+-- username VARCHAR(50) REFERENCES users (username),
 entry_title TEXT,
 entry_date DATE,
 raw_text TEXT,
